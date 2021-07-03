5d90221619aa675bc995488d350a908d109ddaa3
weq <2465935311@qq.com>
2021-07-03 16:05:32 +0800

    对象转json字符串

---
+++ public/json.go
@@ -0,0 +1,22 @@
+package public
+
+import "github.com/json-iterator/go"
+
+var (
+	json              = jsoniter.ConfigCompatibleWithStandardLibrary
+	JSONMarshal       = json.Marshal
+	JSONUnmarshal     = json.Unmarshal
+	JSONMarshalIndent = json.MarshalIndent
+	JSONNewDecoder    = json.NewDecoder
+	JSONNewEncoder    = json.NewEncoder
+)
+
+// JSONMarshalToString JSON编码为字符串
+func JSONMarshalToString(v interface{}) string {
+	s, err := jsoniter.MarshalToString(v)
+	if err != nil {
+		return ""
+	}
+
+	return s
+}
